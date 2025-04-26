5ac8ecfd72fbd8139a3558020c9c60b03b57b4a4
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2025-04-26 15:19:45 +0200

    FieldGrid+FieldCell: Replace wheat with the new one

--- src/Game/FieldCell.cpp
+++ src/Game/FieldCell.cpp
@@ -1,5 +1,6 @@
 #include "FieldCell.h"
 
+#include "AK/AK.h"
 #include "SceneSerializer.h"
 #include "imgui_extensions.h"
 
@@ -39,12 +40,17 @@ void FieldCell::awake()
     i32 wheat_count = 16;
     for (i32 i = 0; i < wheat_count; i++)
     {
-        auto wheat = SceneSerializer::load_prefab("Wheat");
-        float x = (i % 4) * 0.18f - m_cell_size_and_offset * 0.5f; // X position (8 per row)
-        float z = (i / 4) * 0.18f - m_cell_size_and_offset * 0.5f; // Z position (new row every 8)
+        float const random_offset_1 = AK::random_float(0.45f, 0.55f);
+        float const random_offset_2 = AK::random_float(0.45f, 0.55f);
+
+        auto const wheat = AK::random_bool() ? SceneSerializer::load_prefab("Wheat") : SceneSerializer::load_prefab("Wheat2");
+        float x = (i % 4) * 0.18f - m_cell_size_and_offset * random_offset_1; // X position (8 per row)
+        float z = (i / 4) * 0.18f - m_cell_size_and_offset * random_offset_2; // Z position (new row every 8)
         wheat->transform->set_local_position({x, 0.0f, z});
         wheat->transform->set_parent(entity->transform);
         wheat->is_serialized = false;
+        glm::vec3 const e = wheat->transform->get_euler_angles();
+        wheat->transform->set_euler_angles({e.x, AK::random_float(0.0f, 360.0f), e.z});
         m_wheats.push_back(wheat->get_component<Wheat>());
     }
 }
