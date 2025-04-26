5e3cd574d030a3395a61daeba47bfcc348332ce3
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2025-04-26 17:55:14 +0200

    Cow: Add cow rotation during movement

--- src/Komiks/Cow.h
+++ src/Komiks/Cow.h
@@ -19,6 +19,7 @@ public:
     virtual void awake() override;
     virtual void start() override;
     virtual void update() override;
+    virtual void fixed_update() override;
     virtual void on_collision_enter(std::shared_ptr<Collider2D> const& other) override;
 
     std::weak_ptr<CowManager> cow_manager = {};
